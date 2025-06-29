8361f666aea608fba634b990a979b4cb7793c273
René Vaessen <rene.vaessen@gmail.com>
2025-06-29 05:11:04 +0200

    Release 1.194.2025

--- Functions/GenXdev.Coding.Git/Get-GitChangedFiles.ps1
+++ Functions/GenXdev.Coding.Git/Get-GitChangedFiles.ps1
@@ -13,7 +13,7 @@ function Get-GitChangedFiles {
 
     .EXAMPLE
         Get-GitChangedFile
-        Returns relative paths like ".\Modules\GenXdev.AI\1.192.2025\README.md"
+        Returns relative paths like ".\Modules\GenXdev.AI\1.194.2025\README.md"
 
     .EXAMPLE
         Get-GitChangedFile -PassThru
