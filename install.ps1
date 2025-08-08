cff14854859685495dd495e6ba9bb639ca116dbb
René Vaessen <rene.vaessen@gmail.com>
2025-08-08 04:42:17 +0200

    Release 1.236.2025

--- install.ps1
+++ install.ps1
@@ -25,11 +25,11 @@ try {
             try {
                 [System.IO.Directory]::Move(
                     "$localPath\$($version.Name)",
-                    "$localPath\1.234.2025"
+                    "$localPath\1.236.2025"
                 )
             }
             catch {
-                GenXdev.FileSystem\Start-RoboCopy "$localPath\$($version.Name)" "$localPath\1.234.2025"
+                GenXdev.FileSystem\Start-RoboCopy "$localPath\$($version.Name)" "$localPath\1.236.2025"
             }
         }
     }
