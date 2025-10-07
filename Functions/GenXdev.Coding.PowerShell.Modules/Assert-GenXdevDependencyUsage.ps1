aa928559873aadb23397ab9c91b79ba4b0ab2ae4
René Vaessen <rene.vaessen@gmail.com>
2025-10-07 10:13:31 +0200

    Release 1.296.2025

--- Functions/GenXdev.Coding.PowerShell.Modules/Assert-GenXdevDependencyUsage.ps1
+++ Functions/GenXdev.Coding.PowerShell.Modules/Assert-GenXdevDependencyUsage.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.Coding.PowerShell.Modules
 Original cmdlet filename  : Assert-GenXdevDependencyUsage.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.292.2025
+Version                   : 1.296.2025
 ################################################################################
 MIT License
 
